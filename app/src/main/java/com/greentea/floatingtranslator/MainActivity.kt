99720f63f29fe61122f2c43597050dc0fbc882ef
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-14 22:50:31 +0900

    Make MainActivity

--- app/src/main/java/com/greentea/floatingtranslator/MainActivity.kt
+++ app/src/main/java/com/greentea/floatingtranslator/MainActivity.kt
@@ -1,12 +1,43 @@
 package com.greentea.floatingtranslator
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import com.greentea.floatingtranslator.Service.FloatingService
 
 class MainActivity : AppCompatActivity() {
+
+    private val OVERLAY_PERMISSION_REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        checkPermission()
+    }
+
+    fun checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.canDrawOverlays(this)) {
+                val intent = Intent(
+                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse(" package:$packageName")
+                )
+                startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE)
+            } else {
+                startService()
+            }
+        } else {
+            startService()
+        }
+    }
+
+    fun startService(){
+        val floatingService = Intent(applicationContext, FloatingService::class.java)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            startService(floatingService)
+        }
     }
 }
---
+++ app/src/main/java/com/greentea/floatingtranslator/Service/FloatingService.kt
@@ -0,0 +1,12 @@
+package com.greentea.floatingtranslator.Service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class FloatingService : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}

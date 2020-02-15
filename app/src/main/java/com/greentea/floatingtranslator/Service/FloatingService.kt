a1057cd41741fb54104b3050653b57c3c4247f3a
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-15 18:34:15 +0900

    Update FloatingService.kt

--- app/src/main/java/com/greentea/floatingtranslator/Service/FloatingService.kt
+++ app/src/main/java/com/greentea/floatingtranslator/Service/FloatingService.kt
@@ -2,11 +2,41 @@ package com.greentea.floatingtranslator.Service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
+import com.greentea.floatingtranslator.FloatingView.FloatingHeadWindow
 
 class FloatingService : Service() {
 
+    lateinit var floatingHeadWindow: FloatingHeadWindow
+    private var mBinder = LocalBinder()
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return mBinder
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        init()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    fun init(){
+        if(!::floatingHeadWindow.isInitialized){
+            floatingHeadWindow = FloatingHeadWindow(applicationContext).apply {
+                create()
+                createLayoutParams()
+                show()
+            }
+        }
+    }
+
+    inner class LocalBinder: Binder(){
+        fun getService(): FloatingService{
+            return this@FloatingService
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }

e19b6df43921c400a2d14fd7e09c1b62dbb819df
bsgreentea <46317084+bsgreentea@users.noreply.github.com>
2020-02-16 15:01:39 +0900

    Update MainActivity.kt

--- app/src/main/java/com/greentea/floatingtranslator/MainActivity.kt
+++ app/src/main/java/com/greentea/floatingtranslator/MainActivity.kt
@@ -1,43 +1,54 @@
 package com.greentea.floatingtranslator
 
+import android.annotation.TargetApi
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
+import android.widget.Toast
 import com.greentea.floatingtranslator.Service.FloatingService
 
 class MainActivity : AppCompatActivity() {
 
-    private val OVERLAY_PERMISSION_REQUEST_CODE = 1
+    companion object{
+        private const val OVERLAY_PERMISSION_REQUEST_CODE = 123
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkPermission()
+        startFloatingWidgetMaybe()
     }
 
-    fun checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (Settings.canDrawOverlays(this)) {
-                val intent = Intent(
-                    Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
-                    Uri.parse(" package:$packageName")
-                )
-                startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE)
-            } else {
-                startService()
-            }
-        } else {
-            startService()
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == OVERLAY_PERMISSION_REQUEST_CODE && isDrawOverlayAllowed()){
+            Toast.makeText(this, "Granted Permission", Toast.LENGTH_SHORT).show()
+            startFloatingWidgetMaybe()
         }
     }
 
-    fun startService(){
-        val floatingService = Intent(applicationContext, FloatingService::class.java)
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            startService(floatingService)
-        }
+    override fun onDestroy() {
+        stopService(Intent(this, FloatingService::class.java))
+        Toast.makeText(this, "Stopped widget", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
     }
+
+    private fun startFloatingWidgetMaybe(){
+        if(isDrawOverlayAllowed()){
+            startService(Intent(this@MainActivity, FloatingService::class.java))
+            return
+        }
+        requestForDrawingOverAppsPermission()
+    }
+
+    private fun requestForDrawingOverAppsPermission(){
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:$packageName"))
+        startActivityForResult(intent, OVERLAY_PERMISSION_REQUEST_CODE)
+    }
+
+    private fun isDrawOverlayAllowed(): Boolean =
+        Build.VERSION.SDK_INT < Build.VERSION_CODES.M || Settings.canDrawOverlays(this)
 }
